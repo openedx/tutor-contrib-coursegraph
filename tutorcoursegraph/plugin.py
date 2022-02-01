3b3b8d3448708f821af52c5043b9f99a9cfe841b
Kyle McCormick <kyle@tcril.org>
2022-02-01 09:31:49 -0500

    feat!: drop unneccessary cli

--- tutorcoursegraph/plugin.py
+++ tutorcoursegraph/plugin.py
@@ -44,29 +44,3 @@ def patches():
             content = patch_file.read()
             all_patches[name] = content
     return all_patches
-
-
-@click.group(help="CourseGraph administration")
-def command():
-    pass
-
-
-@command.command(help="Refresh CourseGraph from CMS.")
-@click.pass_obj
-@click.option(
-    "-f",
-    "--force",
-    is_flag=True, help="Refresh courses even if they are unchanged.",
-)
-def refresh(context_obj, force: bool):
-    refresh_service = "cms"
-    if force:
-        refresh_script_path = ["coursegraph", "apps", "cms", "refresh-force"]
-    else:
-        refresh_script_path = ["coursegraph", "apps", "cms", "refresh"]
-    context_obj.job_runner().run_job_from_template(refresh_service, *refresh_script_path)
-
-
-@command.command(help="Clear data in CourseGraph.")
-def clear():
-    raise NotImplementedError('TODO: this should clear coursegraph')
